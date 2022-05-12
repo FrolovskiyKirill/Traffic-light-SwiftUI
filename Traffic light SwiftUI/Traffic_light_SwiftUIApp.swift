f5d3ebf19bc346b5dc0a37b2807ed521d3136607
Kirill <frolkv@gmail.com>
2022-05-12 21:29:32 +0300

    Initial Commit

---
+++ Traffic light SwiftUI/Traffic_light_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_light_SwiftUIApp.swift
+//  Traffic light SwiftUI
+//
+//  Created by Кирилл on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_light_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
